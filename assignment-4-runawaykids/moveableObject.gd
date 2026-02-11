edc2663af927af6cec9e480b1d148af25eb5e65b
jow555 <jasperkaddatz@gmail.com>
2026-02-11 13:19:48 -0500

    Update moveableObject.gd

--- assignment-4-runawaykids/moveableObject.gd
+++ assignment-4-runawaykids/moveableObject.gd
@@ -6,7 +6,7 @@ class_name MoveableObject
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(tile_map_layer)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
