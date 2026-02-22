25d54b0d4edcd2bb51b447a00a32e6a179402f38
ril4456 <dylan.j.nunes@gmail.com>
2026-02-21 22:04:49 -0500

    Start of turn limit stuff
    update score and turn labels
    added signal when turn count >=15

--- assignment-4-runawaykids/hud.gd
+++ assignment-4-runawaykids/hud.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 var turn=0
 var score=0
+signal turn_limit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,3 +21,11 @@ func _on_player_turn_end() -> void:
 func _on_kid_captured() -> void:
 	score+=1
 	$ScoreText.text="Score: " + str(score)
+
+func turn_limit_reached():
+	if turn>=15:
+		score=0
+		$ScoreText.text="Score: " + str(score)
+		turn=0
+		$TurnCount.text= "Turn: " + str(turn)
+		turn_limit.emit()
